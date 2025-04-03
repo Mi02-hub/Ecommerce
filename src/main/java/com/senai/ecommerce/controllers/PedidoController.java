42c79011fc681b9ce097aa3c6f9dc5d8e1407798
Mi02-hub <millenaqueiroz834@gmail.com>
2025-04-03 11:30:15 -0300

    :(

---
+++ src/main/java/com/senai/ecommerce/controllers/PedidoController.java
@@ -0,0 +1,27 @@
+package com.senai.ecommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.ecommerce.dto.PedidoDTO;
+import com.senai.ecommerce.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "pedido")
+public class PedidoController {
+	
+	@Autowired
+	PedidoService pedidoService;
+	
+	@PostMapping(value ="/criar")
+	public ResponseEntity<PedidoDTO> inserir(@RequestBody PedidoDTO dto){
+		dto = pedidoService.inserir(dto);
+		return ResponseEntity.status(201).body(dto);
+		
+	}
+
+}
