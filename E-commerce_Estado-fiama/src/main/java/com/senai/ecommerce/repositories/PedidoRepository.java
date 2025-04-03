1f64feff75afe3d55389a28f07ac2123a17ee751
Millena Queiroz <millenaqueiroz834@gmail.com>
2025-04-03 11:36:27 -0300

    Delete E-commerce_Estado-fiama directory

--- E-commerce_Estado-fiama/src/main/java/com/senai/ecommerce/repositories/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.ecommerce.entities.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-}
--- E-commerce_Estado-fiama/src/main/java/com/senai/ecommerce/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.ecommerce.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
